e0ff3c65092ba3fd69db1a7afe29601a1f37122b
Marcelo H. Carneiro <marcelo@devhaus.ie>
2017-05-19 16:23:08 +0100

    Removing brand image

--- src/components/NavHead.vue
+++ src/components/NavHead.vue
@@ -1,6 +1,5 @@
 <template lang="html">
     <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-        <img class="logo" src="../assets/beate-uhse.png">
         <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
